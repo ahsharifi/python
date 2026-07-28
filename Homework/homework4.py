3497dcf9cacdcbaa2f36b214c1110e16cbbee8e9
ahsharifi <mr.amirhossein88@gmail.com>
2026-07-28 15:36:42 -0400

    Add: define List, Tuple, Set and Dictionary

---
+++ Homework/homework4.py
@@ -0,0 +1,20 @@
+# [===============| List |===============]
+fruits = ["apple", "banana", "orange"]
+
+# [===============| Tuple |===============]
+games = ("chess", "soccer", "basketball")
+
+# [===============| Set |===============]
+group_members = {"Ali", "Mamad", "Reza", "Amin"}
+
+# [===============| Dictionary |===============]
+user_data = {
+  "firstname": "Amirhossein",
+  "lastname": "Sharifi",
+  "email": "ahsharifidev@gmail.com",
+  "phone": 9038834741,
+  "age": 18,
+  "city": "Tabriz",
+  "major": "Mathematics",
+  "is_admin": True
+}
