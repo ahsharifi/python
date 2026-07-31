3747b4eecf30c9e270f251e779010c3568ee1c30
ahsharifi <ahsharifidev@gmail.com>
2026-07-31 16:45:35 -0400

    Add: get numbers and operation from user

--- Homework/homework5.py
+++ Homework/homework5.py
@@ -15,3 +15,33 @@ def taghsim(num1: float = 0, num2: float = 0):
   if num2 == 0:
     return "Error: Division by zero"
   return num1 / num2
+
+number1 = float(input("Enter the first number: "))
+number2 = float(input("Enter the second number: "))
+
+print("""\033[93m
+Select operation:
+|--1. Jam
+|--2. Tafrigh
+|--3. Zarb
+|--4. Taghsim
+\033[0m  
+""")
+
+choice = input("Enter choice (1/2/3/4): ")
+
+match choice:
+  case "1":
+    result = jam(number1, number2)
+    print(f"\033[92mResult: {result}\033[0m")
+  case "2":
+    result = tafrigh(number1, number2)
+    print(f"\033[92mResult: {result}\033[0m")
+  case "3":
+    result = zarb(number1, number2)
+    print(f"\033[92mResult: {result}\033[0m")
+  case "4":
+    result = taghsim(number1, number2)
+    print(f"\033[92mResult: {result}\033[0m")
+  case _:
+    print("\033[91mInvalid input\033[0m")
