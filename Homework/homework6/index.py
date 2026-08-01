78d53132a83d6af5d4db0343e44c0ed5f9778afd
ahsharifi <ahsharifidev@gmail.com>
2026-08-01 14:30:26 -0400

    [Add]: students List and Menu

---
+++ Homework/homework6/index.py
@@ -0,0 +1,15 @@
+# students list
+students = []
+
+# function to show menu
+def menu():
+  print("""
+[============| METHODS |============]
+|--| 1. Add student |---------------|
+|--| 2. Show student |--------------|
+|--| 3. Search student |------------|
+|--| 4. Remove student |------------|
+|--| 5. Show number of students |---|
+|--| 6. Exit the app |--------------|
+[===================================]
+  """)
